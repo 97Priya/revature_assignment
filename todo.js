2af66b45bd0dd7556f0a3f86720667f42e698b8a
priya <priyaantil11@gmail.com>
2021-07-13 15:26:19 +0530

    completed task

--- todo.js
+++ todo.js
@@ -19,6 +19,7 @@ class Todo {
   
   class TodoService {
      static todos=[];
+
     addTodo(title) { 
         let todo=new Todo(title);
         TodoService.todos.push(todo);
@@ -35,11 +36,11 @@ class Todo {
         todo.completed=true;
     }
     completeAll() {
-        
-       let unCompletedTodos= TodoService.todos.filter(todo=>todo.completed==false);
-       if(unCompletedTodos.length!=0)
-            unCompletedTodos.forEach(todo=>todo.completed=false);
-       
+        if(TodoService.todos.some(todo=>todo.completed==false)){
+            let unCompletedTodos= TodoService.todos.filter(todo=>todo.completed==false);
+            if(unCompletedTodos.length!=0)
+                unCompletedTodos.forEach(todo=>todo.completed=true);
+        }
     }
     deleteTodo(id) {
         let toBedeletedTodo=TodoService.todos.findIndex(todo=>todo.id==id);
@@ -47,7 +48,7 @@ class Todo {
     }
     clearCompleted() {
         let completedTodos=TodoService.todos.filter(todo=>todo.completed==true);
-        TodoService.todos.splice(completedTodos);
+        completedTodos.forEach(todo=>TodoService.todos.splice(completedTodos.indexOf(todo),1));
     }
     viewTodos(flag) {
         if(flag==='ALL'){
@@ -76,10 +77,13 @@ class Todo {
    todoService.addTodo("item-4");
    todoService.addTodo("item-5");
    todoService.addTodo("item-6");
+   console.log("checking my to do list after adding 6 items...................");
    todoService.viewTodos('ALL');
   todoService.editTodo(2, "item-two");
+  console.log("checking my to do list after editing item-2 as item-two...................");
   todoService.viewTodos('ALL');
   todoService.completeTodo(3);
+  console.log("checking my to do list after adding 6 items...................");
   todoService.viewTodos('ALL');
   todoService.deleteTodo(2);
   todoService.viewTodos('ALL');
